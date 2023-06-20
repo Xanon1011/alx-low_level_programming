5f0c783fecf24ea5b46bc403f72a0b904b229ab5
Ahmed samir <ahsm995@gmail.com>
2023-06-20 06:36:11 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * main - Entry point
+  *Return: always 0
+  */
+int main(void)
+{
+	-putchar('_');
+	-putchar('p');
+	-putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
