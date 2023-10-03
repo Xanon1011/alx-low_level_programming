ff060d869f08a22d11916aaf35c3f0b7a8eada37
Xanon1011 <ahsm995@gmail.com>
2023-10-03 03:51:05 -0700

    corrected

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,14 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 /**
   * _puts - prints a string
   * @str: the string to print
   * Return: void
   */
-void _puts(char *str)
+void _puts(char *s)
 {
-	for (; *str != '\0'; str++)
-	{
-		_putchar(*str);
-	}
-	_putchar('\n');
+	puts(s);
 }
